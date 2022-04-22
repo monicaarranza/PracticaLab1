11b83367a78e2143f8ec0b8044704c2c164c672e
Monica <Monica@MONICA-CARRANZA>
2022-04-22 15:15:25 -0600

    El anexo de la clase Alumno

---
+++ src/practicalab1/PracticaLab1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicalab1;
+
+/**
+ *
+ * @author Monica
+ */
+public class PracticaLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Caracteres especiales 
+        \n = salto de linea 
+        */
+        System.out.println("Hola Mundo! "+(10+12.34)+" $ ");
+        System.out.println("- Valor numerico: "+(10+12.34)+" $ \nEstado: "+true+" - Habilitado");
+        System.out.print("Datos de alumno\nCuenta: 22151005\nNombre Alumno: Monica Carranza\nCarrera: Ingenieria en ciencias de datos\n");
+                
+    }
+
+}
